e87e0f0f55123f6c0fba8ad50e92f561fbe09bf7
Jeff Austin & Jordan Johansen <student@epicodus.com>
2015-09-15 16:27:36 -0700

    Add jQuery and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,43 @@ Ticket.prototype.ticketPrice = function() {
         total += 2;
     }
 
-    if(this.movieTime < 4) {
+    if(this.movieTime < 4.5) {
         total -= 2;
     }
 
-    if(this.age > 60) {
+    if(this.age) {
         total -= 2;
     }
 
     return total;
 }
+
+function resetFields() {
+    $("input#movie-title").val("");
+    $("input#age").val("");
+    $("input#movie-time").val("");
+}
+
+$(document).ready(function() {
+    $("form#new-ticket").submit(function(event) {
+        event.preventDefault();
+
+        var movieName = $("select#movie-title").val();
+        var movieTime = parseInt($("select#movie-time").val());
+        var age = parseInt($("select#age").val());
+        if(movieName.charAt(movieName.length - 1) === "*") {
+            var firstRun = true;
+        } else {
+            var firstRun = false;
+        }
+
+        var newTicket = new Ticket(movieName, firstRun, movieTime, age);
+
+        $("#show-ticket").show();
+        $(".movie").text(newTicket.movieName.replace(/\*$/, ''));
+        $(".showtime").text(newTicket.movieTime + ":00");
+        $(".price").text(newTicket.ticketPrice());
+
+        resetFields();
+    });
+});
